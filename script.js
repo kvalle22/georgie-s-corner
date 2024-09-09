5ca7624226e691fa8b940016e646c3d246713e2b
kaze <kcvj_2295@hotmail.com>
2024-09-09 17:46:19 -0500

    Initial changes
    Initial changes

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevents form from submitting
+    alert("Form submitted successfully!");
+});
